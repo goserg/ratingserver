7bb0ca14ba0e0f26ed24727fd874b93877c54331
goserg <gor-serg@bk.ru>
2023-05-01 10:03:25 +0300

    added pepper to password hashing

--- auth/service/config.go
+++ auth/service/config.go
@@ -1,9 +1,10 @@
 package service
 
 type Config struct {
-	SqliteFile   string `toml:"sqlite_file"`
-	Token        string `toml:"token"`
-	Expiration   string `toml:"expiration"`
-	RootPassword string `toml:"root_password"`
-	Roles        string `toml:"roles"` // TODO create roles on first start
+	SqliteFile     string   `toml:"sqlite_file"`
+	Token          string   `toml:"token"`
+	Expiration     string   `toml:"expiration"`
+	RootPassword   string   `toml:"root_password"`
+	PasswordPepper string   `toml:"password_pepper"`
+	Roles          []string `toml:"roles"` // TODO create roles on first start
 }
