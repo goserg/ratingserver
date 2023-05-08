6a3b474acedfcaf320bda8f874384f1e20727c19
goserg <gor-serg@bk.ru>
2023-05-08 09:15:07 +0300

    :recycle: add selectors package

---
+++ tests/selectors/selectors.go
@@ -0,0 +1,21 @@
+package sel
+
+const (
+	Logo = ".brand-logo"
+
+	NewPlayerFormName   = "#new-player-form-name"
+	NewPlayerFormSubmit = "#new-player-form-submit"
+
+	NewMatchFormWinner = "#new-match-form-winner"
+	NewMatchFormLoser  = "#new-match-form-loser"
+	NewMatchFormDraw   = "#new-match-form-draw"
+	NewMatchFormSubmit = "#new-match-form-submit"
+
+	PlayerListRow     = "#player-list-row"
+	PlayerListRowName = "#player-list-row-name"
+	PlayerListRowLink = PlayerListRow + " a"
+
+	SignInFormUsername = "#username-field"
+	SignInFormPass     = "#password-field"
+	SignIngFormSubmit  = "#signin-form-submit"
+)
